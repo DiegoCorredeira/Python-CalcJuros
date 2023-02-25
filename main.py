c07973fe4baf494178b88c7f32a9eb32a017a828
Diego <dgcostapinto@gmail.com>
2023-02-25 12:49:04 -0300

    corrigindo variavel

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ taxa_juros = float(input('Informe a taxa de juros: '))
 meses = int(input('Informe a quantidade de vezes em que deseja pagar: '))
 decisao = str(input('O juros é simples ou composto? S ou C: ')).lower()
 
-JuroSimples = valor * ((taxa_juros / 100) * meses)
-parcelaS = (valor + JuroSimples) / meses
-valorJuroSimples = JuroSimples + valor
+juroSimples = valor * ((taxa_juros / 100) * meses)
+parcelaS = (valor + juroSimples) / meses
+valorJuroSimples = juroSimples + valor
 #############################################
 jurosComposto = valor * ((1 + taxa_juros / 100) ** meses - 1)
 parcelaC = (valor + jurosComposto) / meses
 valorJurosComposto = jurosComposto + valor
 
 if decisao == 's':
-    print(f'O juros simples do plano contratado é de R${JuroSimples:.2f}, somando {valorJuroSimples:.2f} e o valor '
+    print(f'O juros simples do plano contratado é de R${juroSimples:.2f}, somando {valorJuroSimples:.2f} e o valor '
           f'parcelado em {meses}x é de R${parcelaS:.2f} por mês ')
 if decisao == 'c':
     print(f'O juros composto do plano contratado é de R${jurosComposto:.2f}, somando {valorJurosComposto:.2f} e o '
